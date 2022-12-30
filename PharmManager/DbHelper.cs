35127bb439e95bd1ed7321b46c11a4f7f71c3161
vshimansky <vshimansky@gmail.com>
2022-12-30 07:17:45 +0300

    Add project files.

---
+++ PharmManager/DbHelper.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmManager
+{
+    public static class DbHelper
+    {
+        private const string _sqlAddGood = @"INSERT INTO dbo.Goods (gdName) VALUES('{0}'); 
+                                             SELECT @@ROWCOUNT;";
+        private const string _sqlListGoods = @"SELECT gdId Код, gdName Название
+                                               FROM dbo.Goods WITH(NOLOCK) ORDER BY gdName";
+        private const string _sqlDeleteGood = @"BEGIN TRAN; 
+                                                  DELETE FROM dbo.Batches WHERE bhGoodId = {0};
+                                                  DELETE FROM dbo.Goods WHERE gdId = {0};
+                                                COMMIT;";
+        private const string _sqlAddPharmacy = @"INSERT INTO dbo.Pharmacies (phName, phAddress, phPhone) 
+                                                 VALUES('{0}','{1}','{2}'); 
+                                                 SELECT @@ROWCOUNT;";
+        private const string _sqlListPharmacies = @"SELECT phId Код, phName Название, phAddress Адрес, phPhone Телефон
+                                                    FROM dbo.Pharmacies WITH(NOLOCK) ORDER BY phName";
+        private const string _sqlDeletePharmacy = @"BEGIN TRAN; 
+                                                      DELETE FROM dbo.Batches WHERE bhStockId IN 
+                                                        (SELECT stId FROM dbo.Stocks WHERE stPharmId = {0});
+                                                      DELETE FROM dbo.Stocks WHERE stPharmId = {0};
+                                                      DELETE FROM dbo.Pharmacies WHERE phId = {0};
+                                                    COMMIT;";
+        private const string _sqlListPharmGoods = @"SELECT gd.gdName Название, SUM(bh.bhCount) Количество
+                                                    FROM dbo.Batches bh WITH(NOLOCK)
+                                                    LEFT JOIN dbo.Goods gd WITH(NOLOCK) ON gd.gdId = bh.bhGoodId
+                                                    LEFT JOIN dbo.Stocks st WITH(NOLOCK) ON st.stId = bh.bhStockId
+                                                    WHERE st.stPharmId={0}
+                                                    GROUP BY gd.gdId, gd.gdName;";
+        private const string _sqlAddStock = @"IF EXISTS(SELECT 1 FROM dbo.Pharmacies WHERE phId = {1})
+                                                INSERT INTO dbo.Stocks(stName,stPharmId)
+                                                VALUES('{0}', {1});
+                                              SELECT @@ROWCOUNT;";
+        private const string _sqlListStocks = @"SELECT st.stId Код, st.stName Название, ph.phName Аптека
+                                                FROM dbo.Stocks st WITH(NOLOCK)
+                                                LEFT JOIN dbo.Pharmacies ph WITH(NOLOCK) ON ph.phId = st.stPharmId
+                                                ORDER BY ph.phName, st.stName";
+        private const string _sqlDeleteStock = @"BEGIN TRAN; 
+                                                    DELETE FROM dbo.Batches WHERE bhStockId = {0};
+                                                    DELETE FROM dbo.Stocks WHERE stId = {0};
+                                                 COMMIT;";
+        private const string _sqlAddBatch = @"IF EXISTS(SELECT 1 FROM dbo.Stocks WHERE stId = {0})
+                                                 AND EXISTS(SELECT 1 FROM dbo.Goods WHERE gdId = {1})
+                                                    INSERT INTO dbo.Batches(bhStockId,bhGoodId,bhCount)
+                                                    VALUES({0}, {1}, {2});
+                                              SELECT @@ROWCOUNT;";
+        private const string _sqlListBatches = @"SELECT bh.bhId Код, st.stName Склад, gd.gdName Товар, bh.bhCount Количествово
+                                                FROM dbo.Batches bh WITH(NOLOCK)
+                                                LEFT JOIN dbo.Stocks st WITH(NOLOCK) ON st.stId = bh.bhStockId
+                                                LEFT JOIN dbo.Goods gd WITH(NOLOCK) ON gd.gdId = bh.bhGoodId
+                                                ORDER BY st.stName, gd.gdName";
+        private const string _sqlDeleteBatch = @"DELETE FROM dbo.Batches WHERE bhId = {0}";
+        public static string DbError;
+        private static SqlConnection _connect;
+        public static bool OpenConnection()
+        {
+            bool result = true;
+            string connectionString = GetConnectionString();
+            if (!String.IsNullOrWhiteSpace(connectionString))
+            {
+                _connect = new SqlConnection(connectionString);
+                try
+                {
+                    _connect.Open();
+                }
+                catch (Exception exp)
+                {
+                    result = false;
+                    if (_connect.State == ConnectionState.Open)
+                        _connect.Close();
+                    DbError = exp.Message;
+                }
+            }
+            return result;
+        }
+        private static string GetConnectionString()
+        {
+            ConnectionStringSettings settings =
+                ConfigurationManager.ConnectionStrings["Pharm"];
+            return settings?.ConnectionString;
+        }
+        public static void CloseConnection()
+        {
+            if (_connect.State == ConnectionState.Open)
+                _connect.Close();
+        }
+        public static bool AddGood(string name) => ExecuteScalarSql(String.Format(_sqlAddGood, name), 1);
+        public static string ListGoods() => ExecuteReaderSql(_sqlListGoods, 10, 50);
+        public static bool DeleteGood(string id) => ExecuteNonQuerySql(String.Format(_sqlDeleteGood, id));
+        public static bool AddPharmacy(string name, string address, string phone) => ExecuteScalarSql(String.Format(_sqlAddPharmacy, name, address, phone), 1);
+        public static string ListPharmacies() => ExecuteReaderSql(_sqlListPharmacies, 10, 50, 50, 20);
+        public static bool DeletePharmacy(string id) => ExecuteNonQuerySql(String.Format(_sqlDeletePharmacy, id));
+        public static string ListPharmGoods(string id) => ExecuteReaderSql(String.Format(_sqlListPharmGoods, id), 50, 20);
+        public static bool AddStockd(string name, string pharmId) => ExecuteScalarSql(String.Format(_sqlAddStock, name, pharmId), 1);
+        public static string ListStocks() => ExecuteReaderSql(_sqlListStocks, 10, 50, 50);
+        public static bool DeleteStock(string id) => ExecuteNonQuerySql(String.Format(_sqlDeleteStock, id));
+        public static bool AddBatch(string stockId, string goodId, string count) => ExecuteScalarSql(String.Format(_sqlAddBatch, stockId, goodId, count), 1);
+        public static string ListBatches() => ExecuteReaderSql(_sqlListBatches, 10, 50, 50, 20);
+        public static bool DeleteBatch(string id) => ExecuteNonQuerySql(String.Format(_sqlDeleteBatch, id));
+        private static bool ExecuteScalarSql(string sqlText, int CheckResult)
+        {
+            DbError = "";
+            bool result = true;
+            try
+            {
+                SqlCommand command = new SqlCommand(sqlText, _connect);
+                result = ((int)command.ExecuteScalar() == CheckResult);
+            }
+            catch (Exception exp)
+            {
+                DbError = exp.Message;
+                result = false;
+            }
+            return result;
+        }
+        private static bool ExecuteNonQuerySql(string sqlText)
+        {
+            DbError = "";
+            bool result = true;
+            try
+            {
+                SqlCommand command = new SqlCommand(sqlText, _connect);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception exp)
+            {
+                DbError = exp.Message;
+                result = false;
+            }
+            return result;
+        }
+        private static string ExecuteReaderSql(string sqlText, params int[] spaces)
+        {
+            string result = "";
+            try
+            {
+                SqlCommand command = new SqlCommand(sqlText, _connect);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        List<int> spaces2 = new List<int>();
+                        spaces2.AddRange(spaces);
+                        if (spaces.Length < reader.FieldCount)
+                            spaces2.AddRange(Enumerable.Repeat<int>(50, reader.FieldCount - spaces.Length));
+                        for (int i = 0; i < reader.FieldCount; i++)
+                            result += SetCorrectLength(reader.GetName(i), spaces2[i]);
+                        result += Environment.NewLine;
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                                result += SetCorrectLength(reader[i].ToString(), spaces2[i]);
+                            result += Environment.NewLine;
+                        }
+                    }
+                    else
+                        result = "В базе нет записей";
+                }
+            }
+            catch (Exception exp)
+            {
+                result = "Произошла ошибка в приложении:" + Environment.NewLine;
+                result += exp.Message;
+            }
+            return result;
+        }
+        private static string SetCorrectLength(string data, int num)
+        {
+            return data.Length <= num ? data.PadRight(num, ' ') : data.Substring(0, num);
+        }
+    }
+}
---
+++ PharmManager/Program.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ShouldContinue = DbHelper.OpenConnection();
+            if (!ShouldContinue)
+                ShowDbError(true);
+            char input;
+
+            while(ShouldContinue)
+            {
+                Console.Clear();
+                ShowMainPromt();
+                input = Console.ReadKey().KeyChar;
+                if (input == '0')
+                    ShouldContinue = false;
+                else if (input == '1')
+                    ShouldContinue = ManageGoods();
+                else if (input == '2')
+                    ShouldContinue = ManagePharms();
+                else if (input == '3')
+                    ShouldContinue = ManagStockss();
+                else if (input == '4')
+                    ShouldContinue = ManagBatches();
+            }
+        }
+        static void ShowMainPromt()
+        {
+            Console.WriteLine("Введите номер для выбора режима");
+            Console.WriteLine("1 - работа с товарами");
+            Console.WriteLine("2 - работа с аптеками");
+            Console.WriteLine("3 - работа со складами");
+            Console.WriteLine("4 - работа с партиями товаров");
+            Console.WriteLine("0 - выход из приложения");
+        }
+        #region Товары
+        static bool ManageGoods()
+        {
+            bool ShouldContinue = true;
+            Console.Clear();
+            char input = '9';
+            while (ShouldContinue)
+            {
+                ShowGoodsPromt();
+                input = Console.ReadKey().KeyChar;
+                if (input == '0' || input == '4')
+                    ShouldContinue = false;
+                else if (input == '1')
+                    AddGood();
+                else if (input == '2')
+                    DeleteGood();
+                else if (input == '3')
+                    ListGood();
+            }
+            return input != '0';
+        }
+        static void ShowGoodsPromt()
+        {
+            Console.WriteLine("Работа с товарами");
+            Console.WriteLine("Введите номер для выбора действия");
+            Console.WriteLine("1 - добавить товар");
+            Console.WriteLine("2 - удалить товар");
+            Console.WriteLine("3 - список товаров");
+            Console.WriteLine("4 - выход в главное меню");
+            Console.WriteLine("0 - выход из программы");
+        }
+        static void AddGood()
+        {
+            Console.WriteLine();
+            Console.Write("Введите название товара: ");
+            string name = Console.ReadLine();
+            Console.Clear();
+            if (DbHelper.AddGood(name))
+                Console.WriteLine("Товар успешно добавлен");
+            else
+                ShowDbError();
+            Console.WriteLine();
+        }
+        static void DeleteGood()
+        {
+            Console.WriteLine();
+            Console.Write("Введите код товара: ");
+            string id = Console.ReadLine();
+            id = GetDigitalInput(id, "товара");
+            Console.Clear();
+            if (DbHelper.DeleteGood(id))
+                Console.WriteLine("Товар успешно удален");
+            else
+                ShowDbError();
+            Console.WriteLine();
+        }
+        static void ListGood()
+        {
+            Console.Clear();
+            Console.Write(DbHelper.ListGoods());
+            Console.WriteLine();
+        }
+        #endregion
+        #region Аптеки
+        static bool ManagePharms()
+        {
+            bool ShouldContinue = true;
+            Console.Clear();
+            char input = '9';
+            while (ShouldContinue)
+            {
+                ShowPharmPromt();
+                input = Console.ReadKey().KeyChar;
+                if (input == '0' || input == '5')
+                    ShouldContinue = false;
+                else if (input == '1')
+                    AddPharm();
+                else if (input == '2')
+                    DeletePharm();
+                else if (input == '3')
+                    ListPharmacies();
+                else if (input == '4')
+                    ListPharmGoods();
+            }
+            return input != '0';
+        }
+        static void ShowPharmPromt()
+        {
+            Console.WriteLine("Работа с аптеками");
+            Console.WriteLine("Введите номер для выбора действия");
+            Console.WriteLine("1 - добавить аптеку");
+            Console.WriteLine("2 - удалить аптеку");
+            Console.WriteLine("3 - список аптек");
+            Console.WriteLine("4 - список товаров в аптеке");
+            Console.WriteLine("5 - выход в главное меню");
+            Console.WriteLine("0 - выход из программы");
+        }
+        static void AddPharm()
+        {
+            Console.WriteLine();
+            Console.Write("Введите название аптеки: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите адрес аптеки: ");
+            string address = Console.ReadLine();
+            Console.Write("Введите телефон аптеки: ");
+            string phone = Console.ReadLine();
+            Console.Clear();
+            if (DbHelper.AddPharmacy(name, address, phone))
+                Console.WriteLine("Аптека успешно добавлена");
+            else
+                ShowDbError();
+            Console.WriteLine();
+        }
+        static void DeletePharm()
+        {
+            Console.WriteLine();
+            Console.Write("Введите код аптеки: ");
+            string id = Console.ReadLine();
+            id = GetDigitalInput(id, "аптеки");
+            Console.Clear();
+            if (DbHelper.DeletePharmacy(id))
+                Console.WriteLine("Аптека успешно удален");
+            else
+                ShowDbError();
+            Console.WriteLine();
+        }
+        static void ListPharmacies()
+        {
+            Console.Clear();
+            Console.Write(DbHelper.ListPharmacies());
+            Console.WriteLine();
+        }
+        static void ListPharmGoods()
+        {
+            Console.WriteLine();
+            Console.Write("Введите код аптеки: ");
+            string id = Console.ReadLine();
+            id = GetDigitalInput(id, "аптеки");
+            Console.Clear();
+            Console.Write(DbHelper.ListPharmGoods(id));
+            Console.WriteLine();
+        }
+        #endregion
+        #region Склады
+        static bool ManagStockss()
+        {
+            bool ShouldContinue = true;
+            Console.Clear();
+            char input = '9';
+            while (ShouldContinue)
+            {
+                ShowStockPromt();
+                input = Console.ReadKey().KeyChar;
+                if (input == '0' || input == '4')
+                    ShouldContinue = false;
+                else if (input == '1')
+                    AddStock();
+                else if (input == '2')
+                    DeleteStock();
+                else if (input == '3')
+                    ListStocks();
+            }
+            return input != '0';
+        }
+        static void ShowStockPromt()
+        {
+            Console.WriteLine("Работа со складами");
+            Console.WriteLine("Введите номер для выбора действия");
+            Console.WriteLine("1 - добавить склад");
+            Console.WriteLine("2 - удалить склад");
+            Console.WriteLine("3 - список всех складов");
+            Console.WriteLine("4 - выход в главное меню");
+            Console.WriteLine("0 - выход из программы");
+        }
+        static void AddStock()
+        {
+            Console.WriteLine();
+            Console.Write("Введите название склада: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите код аптеки для склада: ");
+            string id = Console.ReadLine();
+            id = GetDigitalInput(id, "аптеки");
+            Console.Clear();
+            if (DbHelper.AddStockd(name, id))
+                Console.WriteLine("Товар успешно добавлен");
+            else
+                ShowDbError();
+            Console.WriteLine();
+        }
+        static void DeleteStock()
+        {
+            Console.WriteLine();
+            Console.Write("Введите код склада: ");
+            string id = Console.ReadLine();
+            id = GetDigitalInput(id, "склада");
+            Console.Clear();
+            if (DbHelper.DeleteStock(id))
+                Console.WriteLine("Склад успешно удален");
+            else
+                ShowDbError();
+            Console.WriteLine();
+        }
+        static void ListStocks()
+        {
+            Console.Clear();
+            Console.Write(DbHelper.ListStocks());
+            Console.WriteLine();
+        }
+        #endregion
+        #region Партии
+        static bool ManagBatches()
+        {
+            bool ShouldContinue = true;
+            Console.Clear();
+            char input = '9';
+            while (ShouldContinue)
+            {
+                ShowBatchPromt();
+                input = Console.ReadKey().KeyChar;
+                if (input == '0' || input == '4')
+                    ShouldContinue = false;
+                else if (input == '1')
+                    AddBatch();
+                else if (input == '2')
+                    DeleteBatch();
+                else if (input == '3')
+                    ListBatches();
+            }
+            return input != '0';
+        }
+        static void ShowBatchPromt()
+        {
+            Console.WriteLine("Работа с партиями товаров");
+            Console.WriteLine("Введите номер для выбора действия");
+            Console.WriteLine("1 - добавить партию товара");
+            Console.WriteLine("2 - удалить партию товара");
+            Console.WriteLine("3 - список всех партий");
+            Console.WriteLine("4 - выход в главное меню");
+            Console.WriteLine("0 - выход из программы");
+        }
+        static void AddBatch()
+        {
+            Console.WriteLine();
+            Console.Write("Введите код товара в партии: ");
+            string goodId = Console.ReadLine();
+            goodId = GetDigitalInput(goodId, "товара");
+            Console.Write("Введите код склада для партии: ");
+            string stockId = Console.ReadLine();
+            stockId = GetDigitalInput(stockId, "склада");
+            Console.Write("Введите количество товара в партии: ");
+            string count = Console.ReadLine();
+            count = GetDigitalInput(count, "", "Кол-во должно выражаться целым числом: ");
+            Console.Clear();
+            if (DbHelper.AddBatch(stockId, goodId, count))
+                Console.WriteLine("Партия успешно добавлена");
+            else
+                ShowDbError();
+            Console.WriteLine();
+        }
+        static void DeleteBatch()
+        {
+            Console.WriteLine();
+            Console.Write("Введите код партии: ");
+            string id = Console.ReadLine();
+            id = GetDigitalInput(id, "партии");
+            Console.Clear();
+            if (DbHelper.DeleteBatch(id))
+                Console.WriteLine("Партия успешно удалена");
+            else
+                ShowDbError();
+            Console.WriteLine();
+        }
+        static void ListBatches()
+        {
+            Console.Clear();
+            Console.Write(DbHelper.ListBatches());
+            Console.WriteLine();
+        }
+        #endregion
+        #region Вспомогателные методы
+        static void ShowDbError(bool waitReact = false)
+        {
+            Console.WriteLine("В работе приложения произошла ошибка:");
+            Console.WriteLine(DbHelper.DbError);
+            Console.WriteLine();
+            if (waitReact)
+            {
+                Console.WriteLine("Для завершения работы нажмите любую клавишу");
+                Console.Read();
+            }
+        }
+        static string GetDigitalInput(string input, string type, string message = "")
+        {
+            string result = input;
+            while(String.IsNullOrWhiteSpace(result) || result.Any(x => !char.IsDigit(x)))
+            {
+                if (String.IsNullOrWhiteSpace(message))
+                    Console.Write($"Введите код {type} (должен быть числом):");
+                else
+                    Console.Write(message);
+                result = Console.ReadLine().Trim();
+            }
+            return result;
+        }
+        #endregion
+    }
+}
